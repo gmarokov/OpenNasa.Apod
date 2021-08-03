9a88960d7d7189b8f2c43fc3bd8deae797ed6eb1
georgi.marokov <georgi.marokov@gmail.com>
2021-08-04 00:12:06 +0300

    Switch to func

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,14 +1,10 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace OpenNasa.Apod.Client
+namespace Client
 {
     public class Program
     {
@@ -17,7 +13,7 @@ namespace OpenNasa.Apod.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
