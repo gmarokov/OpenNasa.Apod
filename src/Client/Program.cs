4a457b35f0f27864091c14ab106eb961797ed045
Georgi Marokov <georgi.marokov@tribalgroup.com>
2021-09-10 10:12:39 +0100

    Statically set the culture to en

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using System.Globalization;
 
 namespace OpenNasa.Apod.Client
 {
@@ -14,6 +15,9 @@ namespace OpenNasa.Apod.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
 
             await builder.Build().RunAsync();
         }
